18dde85b9d7a31908fea205c60f800e194b12297
fermasia <fermasia@hotmail.com>
2014-11-15 00:26:37 -0300

    Added Intelliactive, Intellidemand, Ondemandplus, Smartassfalcon & weathley governors

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -382,6 +382,21 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
+extern struct cpufreq_governor cpufreq_gov_intelliactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intelliactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
+extern struct cpufreq_governor cpufreq_gov_intellidemand;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intellidemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASSFALCON)
+extern struct cpufreq_governor cpufreq_gov_smartassfalcon;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartassfalcon)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDPLUS)
+extern struct cpufreq_governor cpufreq_gov_ondemandplus;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemandplus)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WEATHLEY)
+extern struct cpufreq_governor cpufreq_gov_weathley;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_weathley)
 #endif
 
 
