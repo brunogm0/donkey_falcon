9a332224a3bdbd36a2ed8af8d2c1a7c0f450b278
fermasia <fermasia@hotmail.com>
2014-11-29 14:39:19 -0300

    ramdisk: correct derp and add custom ramdisk settings

---
+++ ramdisk/init.donkey.sh
@@ -0,0 +1,29 @@
+#!/system/bin/sh
+# Copyright (c) 2014, DonkeyKang <fermasia@hotmail.com>
+# Copyright (c) 2009-2014, The Linux Foundation. All rights reserved.
+#
+#!/sbin/bbx sh
+#
+# Checks for power hal and makes a backup
+#
+
+# Interactive tunables;
+echo 30000 1094000:40000 1190000:20000 > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
+echo 85 1094000:80 1190000:95 > /sys/devices/system/cpu/cpufreq/interactive/target_loads
+echo 30000 1094000:20000 1190000:10000 > /sys/devices/system/cpu/cpufreq/interactive/timer_rate
+echo 50000 1094000:30000 > /sys/devices/system/cpu/cpufreq/interactive/timer_slack
+
+# Disable PoweHAL from the rom-side;
+/sbin/bbx mount -o rw,remount /system;
+if /sbin/bbx [ -e /system/lib/hw/power.msm8226.so ]; then
+  /sbin/bbx [ -e /system/lib/hw/power.msm8226.so.backup ] || /sbin/bbx cp /system/lib/hw/power.msm8226.so /system/lib/hw/power.msm8226.so.backup;
+  /sbin/bbx [ -e /system/lib/hw/power.msm8226.so ] && /sbin/bbx rm -f /system/lib/hw/power.msm8226.so;
+fi;
+
+# Enable init.d with permissions;
+if /sbin/bbx [ ! -e /system/etc/init.d ]; then
+  /sbin/bbx mkdir /system/etc/init.d;
+  /sbin/bbx chown -R root.root /system/etc/init.d;
+  /sbin/bbx chmod -R 755 /system/etc/init.d;
+fi;
+/sbin/bbx mount -o ro,remount /system;
